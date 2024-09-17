6c11a54b7759ee597abfb7225fd85557c16c62b2
n.boltacheva <natali.looking.for.a.job@gmail.com>
2024-09-17 14:55:57 +0200

    add fontsize and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./docs/*.html"],
   theme: {
@@ -6,7 +5,34 @@ module.exports = {
       'display': ['Commissioner'],
       'body': ['"Commissioner"'],
     },
-    extend: {},
+    fontSize: {
+      sm: '16px',
+      base: '24px',
+      md: '18px',
+      lg: '20px',
+      xl: '36px',
+      '2xl': '48px',
+    },
+    extend: {
+      lineHeight: {
+        'base': '1.2',
+      },
+      colors: {
+        'background': '#fff',
+        'backgroundBlue': '#F6FAFF',
+        'backgroundFooter': '#0A2530',
+        'base': '#000000',
+        'primary': '#FAE01A',
+        'yellow-opacity': '#FFF7BE',
+        'secondary': '#018FA3',
+        'blue-opacity': '#95D8E0',
+        'third': '#955EEA',
+        'fourth': '#4ECA30',
+        'fifth': '#FFE41B',
+        'sixth': '#FF4D94',
+        'seventh': '#FFA724',
+      },
+    },
   },
   plugins: [],
 }
