0afc2c2b2a912f982fa09b1543d016393522e41f
n.boltacheva <natali.looking.for.a.job@gmail.com>
2024-09-17 12:59:13 +0200

    add tailwind and font

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./docs/*.html"],
+  theme: {
+    fontFamily: {
+      'display': ['Commissioner'],
+      'body': ['"Commissioner"'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,11 @@ module.exports = {
                         },
                     }
                 }
+            },
+            {
+                test: /\.css$/i,
+                include: path.resolve(__dirname, 'src'),
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
             }
             ]
     },
