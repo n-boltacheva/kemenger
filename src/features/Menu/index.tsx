24300dc43bcd49fc1bf0ebfb86fab66e6e08f0df
n.boltacheva <natali.looking.for.a.job@gmail.com>
2024-10-02 19:53:57 +0200

    add effect scroll for menu

--- src/features/Menu/index.tsx
+++ src/features/Menu/index.tsx
@@ -4,11 +4,13 @@ import {Link} from "@shared";
 import {Navbar} from "@entities";
 import logo from './logo.png';
 import phone from './phone.png';
+import {useEffect, useRef} from "react";
 
 type Props = {
 
 };
 export default function Menu(props: Props) {
+    const [flagBackground, setBackground] = React.useState<boolean>(false);
     const menu = [
         {
             link: 'about',
@@ -32,15 +34,30 @@ export default function Menu(props: Props) {
         },
     ];
 
+    function scroll() {
+            const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+            if (scrollTop > 80) {
+                setBackground(true);
+            } else {
+                setBackground(false);
+            }
+    }
+
+    useEffect(()=>{
+        window.addEventListener('scroll', scroll);
+        return () => {window.removeEventListener('scroll', scroll);};
+    },[])
+
     return (
         <nav
-            className="flex justify-around items-center sticky top-0 bg-transparent gap-2 z-10">
+            className="flex justify-around items-center sticky top-0 bg-transparent gap-2 z-20"  style={{backgroundColor:flagBackground?'#fff':'transparent'}}>
             <div className="flex items-center">
                 <Link link={'/'}><img src={logo} className={'w-10 sm:w-[4.3rem]'}/></Link>
             </div>
             <Navbar menu={menu}></Navbar>
             <a className={'flex items-center text-sm sm:text-md hover:text-primary gap-1 '} href={'tel:8 (771) 145-98-26'}>
-                <div className={'bg-secondary p-1.5 sm:p-2 rounded-2xl '}><img src={phone} className={'w-2 sm:w-3'}/></div>
+                <div className={'bg-secondary p-1.5 sm:p-2 rounded-2xl'}><img src={phone} className={'w-2 sm:w-3'}/></div>
                 <b>8 (771) 145-98-26</b>
             </a>
         </nav>
